665553306f4f46e6a1ebc23a54d9c52d0ad55d4f
Heather Miller <heather.miller114@gmail.com>
2018-09-05 00:32:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,20 @@
 # Add  code here!
 require 'benchmark'
 
-#def prime?(number)
- # if number <= 1
- #   false
- # elsif number == 2 or number == 3
-  #  true
-  #elsif (2...number-1).any?{|x| number % x == 0}
- #   false
-  #else
- #   true
- # end
-#end
+def prime?(number)
+  if number <= 1
+    false
+  elsif number == 2 or number == 3
+    true
+  elsif (2...number-1).any?{|x| number % x == 0}
+          # '...' is how to iterate over a range
+          # % is the number divided by x has a remained of nothing, meaning it was divisible
+          # and if it was divisible it is not a prime number
+    false
+  else
+    true
+  end
+end
 
 
 def prime?(number)
@@ -23,3 +26,4 @@ def prime?(number)
     true
   end
 end
+# this method gets rid of one 'true' statement, making it shorter
