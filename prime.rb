3f0bb8de5dddfa7afca9f1567fa47d43fff09e28
Heather Miller <heather.miller114@gmail.com>
2018-09-05 00:29:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,25 @@
 # Add  code here!
 require 'benchmark'
 
+#def prime?(number)
+ # if number <= 1
+ #   false
+ # elsif number == 2 or number == 3
+  #  true
+  #elsif (2...number-1).any?{|x| number % x == 0}
+ #   false
+  #else
+ #   true
+ # end
+#end
+
+
 def prime?(number)
   if number <= 1
     false
-  elsif number == 2 or number == 3
-    true
   elsif (2...number-1).any?{|x| number % x == 0}
     false
   else
     true
   end
 end
-
-puts Benchmark.measure{100}
